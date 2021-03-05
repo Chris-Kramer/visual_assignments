0485c7be99e46ffb20d6460f13791545a73fba83
Christoffer Kramer <chris.m.kramer@outlook.dk>
2021-03-05 21:12:37 +0100

    added changes to script

--- assignment-3/src/assignment-3-cmk.py
+++ assignment-3/src/assignment-3-cmk.py
@@ -14,39 +14,51 @@ from PIL import Image
 #Image
 jeff_img = cv2.imread("../data/jefferson_memorial.jpg") 
 
-#find Height and widt
-(width, height) = jeff_img.shape[1], jeff_img.shape[0]
+#Center
+(centerX, centerY) = jeff_img.shape[1] // 2, jeff_img.shape[0] // 2
+    
+#Pixels left and right from center
+x_pixels_left = 750
+x_pixels_right = 700
+    
+#Pixels up and down from center
+y_pixels_up = 750
+y_pixels_down = 1175
+    
+#Start and end point for rectangle
+start_point = ((centerX - x_pixels_left), (centerY - y_pixels_up))
+end_point = ((centerX + x_pixels_right), (centerY + y_pixels_down))
+
 """
 ---------- Main function ----------
 """
 def main():
     """
+    Create line with Region of Interest
+    """
+    
+    #Draw rectangle
+    img_with_ROI = cv2.rectangle(jeff_img.copy(),
+                      start_point,
+                      end_point,
+                      (0,255,0),
+                      2)
+    #save image
+    cv2.imwrite("../output/image_with_ROI.jpg", img_with_ROI)
+    
+    """
     Crop image
     """
-    jeff_img = Image.open("../data/jefferson_memorial.jpg") 
-    # Setting the points for cropped image 
-    # These were found through a combination of intuition and trial and error
-    left = width//4
-    top = height//4
-    right = (width//4)*3
-    bottom = (height//8)*7
-  
-    # Cropped image of above dimension  
-    jeff_img = jeff_img.crop((left, top, right, bottom)) 
-    
-    #Convert cropped image from PIL type to array
-    #This is done because I'm using the PIL library to crop the image.
-    jeff_img = np.array(jeff_img)
-    
-    #Save cropped image
-    cv2.imwrite("../output/image_cropped.jpg", jeff_img)
-    #jeff_img.save("../output/image_cropped.jpg")
+    cropped_image = jeff_img[(centerY - y_pixels_up):(centerY + y_pixels_down),
+                   (centerX - x_pixels_left):(centerX + x_pixels_right)]
+    #Save image
+    cv2.imwrite("../output/image_cropped.jpg", cropped_image)
     
     """
     Blur and canny edge detection
     """
     #Convert to greyscale
-    grey_cropped = cv2.cvtColor(jeff_img, cv2.COLOR_BGR2GRAY)
+    grey_cropped = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
     #Gaussian blur
     blurred = cv2.GaussianBlur(grey_cropped, (5,5), 0)
     #Canny blur
@@ -60,13 +72,14 @@ def main():
                 cv2.RETR_EXTERNAL,
                 cv2.CHAIN_APPROX_SIMPLE)
     #Draw contours
-    image_contours = cv2.drawContours(jeff_img.copy(), #Draw contours on original
+    image_contours = cv2.drawContours(cropped_image.copy(), #Draw contours on original
                         contours, #our list of contours
                          -1, #Which contour to draw (-1 draws all contours)
                         (0,255,0), #Contour color
                          2)
     #Save image
     cv2.imwrite("../output/image_letters.jpg", image_contours)
+    
 #Define behaviour when called from command line
 if __name__ == "__main__":
     main()
